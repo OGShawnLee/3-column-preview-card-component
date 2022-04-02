eeded306e5d22ba98c4b05ee9884db23ad4a6afc
Shawn Lee <changoloco.evg@gmail.com>
2022-04-02 17:43:55 -0600

    refactor: abstract away card rounded and button animation classes

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -3,8 +3,7 @@
   export let color;
 </script>
 
-<section
-  class="p-12 | flex flex-col gap-9 lg:rounded-none bg-{color} first:rounded-t-lg last:rounded-b-lg lg:(first:rounded-t-none last:rounded-b-none first:rounded-l-lg last:rounded-r-lg)">
+<section class="p-12 | flex flex-col gap-9 bg-{color} rounded-first-last">
   <header class="contents">
     <img class="max-w-min" src={icon} alt="" />
     <h2 class="text-4xl font-bold font-shoulders text-white/95 uppercase">
@@ -15,7 +14,7 @@
     <slot name="paragraph" />
   </p>
   <button
-    class="w-fit px-8 py-3 lg:mt-auto | flex items-center justify-center | bg-white/75 hover:(bg-transparent ring-[3px] ring-white/75 text-white/75) rounded-full text-{color}">
+    class="w-fit px-8 py-3 lg:mt-auto | flex items-center justify-center | bg-white/75 button-animation rounded-full text-{color}">
     Learn More
   </button>
 </section>
@@ -23,5 +22,13 @@
 <style>
   .w-fit {
     @apply w-[fit-content];
+  }
+
+  .button-animation {
+    @apply hover:(bg-transparent ring-[3px] ring-white/75 text-white/75) rounded-full;
+  }
+
+  .rounded-first-last {
+    @apply first:rounded-t-lg last:rounded-b-lg lg:(first:rounded-t-none last:rounded-b-none first:rounded-l-lg last:rounded-r-lg);
   }
 </style>
